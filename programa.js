68f11537d21cba8bc1c43798daacb9679cf654d9
Leandro Ramos <Leandro_Ramos@juliabue.com.ar>
2022-03-17 17:51:57 -0300

    se agrego handlHandlebars

--- programa.js
+++ programa.js
@@ -1,149 +1,29 @@
 const fs = require('fs');
 const express = require('express')
+const handlebars = require('express-handlebars')
+
 const productosRouter = require('./routes/productosRouter')
 
 const app = express()
 const PORT = 8080
 
+app.set('view engine', 'hbs')
+app.set('views', './views')
 app.use(express.static('public'))
 
-app.use('/api/productos', productosRouter)
+app.use('/productos', productosRouter)
 
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+        defaultLayout: 'index.hbs',
+    })
+)
 
 const server = app.listen(PORT, () => {
     console.log(`servidor iniciado en el puerto ${server.address().port}`)
 })
-
 server.on('error', (error) => { console.log(`Hubo un error: ${error}`) })
 
-// class Contenedor{
-//     constructor(nombre){
-//         this.nombre = nombre
-//     }
-
-//     async recargar(obj){
-//         try{
-//             const contenido = fs.readFileSync(this.nombre)
-//             const contenido_parsed = JSON.parse(contenido)
-//             fs.writeFileSync('./productos.txt',JSON.stringify([...contenido_parsed,obj],null,2))
-//         }
-//         catch(error){
-//             fs.writeFileSync('./productos.txt',JSON.stringify([{...obj,}],null,2))
-//         }
-//     }
-
-//     async save(obj){
-//         try{
-//             const contenido = fs.readFileSync(this.nombre)
-//             const contenido_parsed = JSON.parse(contenido)
-//             obj["id"] = (contenido_parsed[contenido_parsed.length -1].id) + 1
-//             fs.writeFileSync("./productos.txt",JSON.stringify([...contenido_parsed,obj],null,2))
-//         }
-//         catch(error){
-//             fs.writeFileSync("./productos.txt",JSON.stringify([{...obj,id: 0}],null,2))
-//         }
-//     }
-
-//     getById(id){
-//         try {
-//             const productos = fs.readFileSync(this.nombre, "utf-8")
-//             let productosParsed = JSON.parse(productos)
-//             return JSON.stringify(productosParsed.find(producto => id === producto.id),null,2)
-//         }
-//         catch (error) {
-//             console.log(error)
-//         }
-//     }
-
-//     getAll(){
-//         try {
-//             const contenido = fs.readFileSync(this.nombre, "utf-8")
-//             let datosParsed = JSON.parse(contenido)
-//             return JSON.stringify(datosParsed,null,2)
-//         }
-//         catch (error) {
-//             console.log("Error: ", toString(error))
-//         }
-//     }
-
-//     deleteById(id){
-//         try {
-//             const productos = fs.readFileSync(this.nombre, "utf-8")
-//             let productosarray = JSON.parse(productos)
-//             productosarray.forEach(element => {
-//                 if(element.id === id){
-//                     productosarray.splice(productosarray.indexOf(element),1)
-//                     contenedor.deleteAll()
-//                     contenedor.recargar(productosarray)
-//                     return console.log("Se borro Producto")
-//                 }
-//             });
-            
-//         } catch (error) {
-//             console.log("No se encontro el Producto", error)
-//         }
-//     }
-
-//     deleteAll(){
-//         fs.writeFileSync("./productos.txt","")
-//     }
-    
-//     long(){
-//         try {
-//             const productos = fs.readFileSync("./productos.txt", "utf-8")
-//             let productosarray = JSON.parse(productos)
-//             return productosarray.length
-//         }
-//         catch (error) {
-//             console.log("Error: ", toString(error))
-//         }
-//     }
-// }
-
-
-
-// const contenedor = new Contenedor("./productos.txt")
-
-// contenedor.save({
-//     title: 'martillo',
-//     price: 1170,
-//     thumbnail: 'https://assets.tramontina.com.br/upload/tramon/imagens/GAR/40200029PNM001G.png \n'
-// })
-// contenedor.save({
-//     title: 'destornillador Torx',
-//     price: 580,
-//     thumbnail: 'https://arcencohogar.vtexassets.com/arquivos/ids/300270-500-auto?v=637665764765470000&width=500&height=auto&aspect=true'
-// })
-// contenedor.save({
-//     title: 'pico de loro',
-//     price: 1275,
-//     thumbnail: 'https://arcencohogar.vtexassets.com/arquivos/ids/299158-500-auto?v=637665763525330000&width=500&height=auto&aspect=true'
-// })
-
-// console.log(contenedor.getById(1))
-// console.log(contenedor.getAll())
-// //contenedor.deleteById(1)
-// //contenedor.deleteAll()
-
-
-
-
-// app.get("/prueba", (req, res) => {
-//     res.send(
-//         `hola mundo`
-//         )
-//     })
-
-// app.get("/productos", (req, res) => {
-//         res.send(
-//             `todos los productos: </br> ${contenedor.getAll()}`
-//             )
-//     });
-
-// app.get("/productoRandom", (req, res) => {
-//     let aux = parseInt(Math.random() * contenedor.long() - 1) +1
-//     res.send(
-//     ` <h1> Este es el producto: ${contenedor.getById(aux)}</h1>`)
-// })
-
-
+module.exports = app
--- routes/productosRouter.js
+++ routes/productosRouter.js
@@ -1,4 +1,5 @@
-const express   = require('express')
+const express = require('express')
+const app = require('../programa')
 const productosRouter = express.Router()
 
 const productos = []
@@ -6,9 +7,6 @@ const productos = []
 productosRouter.use(express.urlencoded({ extended: true }))
 productosRouter.use(express.json())
 
-productosRouter.get('/', (req, res) => {
-    res.json(productos)
-})
 
 productosRouter.get('/:id', (req, res) => {
 
@@ -24,24 +22,38 @@ productosRouter.get('/:id', (req, res) => {
     
 })
 
-productosRouter.post('/', (req, res) => {
-    productos.push(req.body)
-    aux = productos.length
-    productos[aux-1].id = aux
-    res.json({message: 'se agrego el nuevo producto'})
+productosRouter.put('/:id', (req, res) => {
+
+    if(isNaN(req.params.id)){
+        res.json({error : 'producto no encontrado' })
+    }
+    else if( req.params.id > productos.length) {
+        res.json({error : 'producto no existe' })
+    }
+    else{
+        productos.splice(req.params.id -1, 1,req.body)
+        res.json({
+            message: 'se moficico el productos'
+        })
+    }
+})
+    
+    productosRouter.post('/', (req, res) => {
+        productos.push(req.body)
+        aux = productos.length
+        productos[aux-1].id = aux
+        res.json({message: 'se agrego el nuevo producto'})
 })
 
-productosRouter.put('/:id', (req, res) => {
-    productos.splice(req.params.id -1, 1,req.body)
-    res.json({
-        message: 'se moficico el productos'
-    })
-})
-productosRouter.delete('/:id', (req, res) => {
-    // productos.splice(req.params.id, 1,'')
-    // res.send( 'se borro el productos ')
+    productosRouter.delete('/:id', (req, res) => {
     productos.splice(req.params.id -1, 1)
     res.send( 'se borro el productos ')
 })
+
+
+productosRouter.get('/', (req,res) => {
+    res.render('datos',productos)   
+})
+
 
 module.exports = productosRouter
